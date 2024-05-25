63c51671f3cbc221405029b388d28ee28356a55e
ysavilov <y.savilov@gmail.com>
2024-05-25 18:21:49 +0400

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,7 +6,7 @@ const regd_users = express.Router();
 let users = [{"username":"dennis","password":"abc"}];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+//write code to check is the username is valid.
     const userMatches = users.filter((user) => user.username === username);
     return userMatches.length > 0;
 }
