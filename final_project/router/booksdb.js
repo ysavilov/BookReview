835c1c2bd1334c017bc506f86972820d33436623
ysavilov <y.savilov@gmail.com>
2024-05-25 18:22:04 +0400

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,4 +1,4 @@
-# books DB
+// books DB
 let books = {
       1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
       2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
